7245fc9171fcd96445ad17e2f829b433ab8ecfe6
Denis <chugunov-official@mail.ru>
2020-05-21 21:14:29 +0300

    dev: del Not and Sign mixin

--- multidb/mixins.py
+++ multidb/mixins.py
@@ -1,32 +1,3 @@
-import logging
-
-from . import symbols as ss
-
-
-class SignMixin:
-    logger = logging.getLogger('sign_mixin')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.sign = 1
-
-    def set_sign(self, sign):
-        if sign == ss.minus_sign:
-            self.sign *= -1
-
-
-class NotMixin:
-    logger = logging.getLogger('not_mixin')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.is_not = False
-
-    def set_not(self, flag=True):
-        if flag:
-            self.is_not = not self.is_not
-
-
 class AsMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -34,3 +5,4 @@ class AsMixin:
 
     def as_(self, name):
         self.short_name = name
+        return self
